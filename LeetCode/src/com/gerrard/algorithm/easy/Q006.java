d4ce26521eb7dd40e8feda2a8ac2ccfa55891087
OEM-QIULIGAN\Administrator <kevin_gerrard@sina.com>
2016-09-22 13:42:41 +0800

    Add Q006

--- LeetCode/src/com/gerrard/algorithm/easy/Q006.java
+++ LeetCode/src/com/gerrard/algorithm/easy/Q006.java
@@ -3,14 +3,40 @@ package com.gerrard.algorithm.easy;
 public class Q006 {
 
 	public static void main(String[] args) {
-		String str = "PAYPALISHIRING";
-		System.out.println(method(str, 3));
+		 String str1 = "PAYPALISHIRING";
+		 System.out.println(method(str1, 3));
+		String str2 = "abcdefghijklmnopqrstuvwxyz";
+		System.out.println(method(str2, 5));
 	}
 
-	// 题目缺陷，只考虑nRows为奇数的情况
 	private static String method(String text, int nRows) {
-		
-
-		return text;
+		char[] array = text.toCharArray();
+		StringBuffer sb = new StringBuffer();
+		// 从上至下/从下至上的标志位
+		int flag = 1;
+		// 按行遍历
+		for (int i = 0; i < nRows; i++) {
+			// 每一行的数据
+			for (int j = 0; i + j < array.length;) {
+				sb.append(array[i + j]);
+				// 极点有特殊的处理方式
+				if (i == 0 || i == nRows - 1) {
+					j += 2 * (nRows - 1);
+				} else {
+					if (flag == 1) {
+						// 从上至下处理
+						j += 2 * (nRows - i - 1);
+					} else {
+						// 从下至上处理
+						j += 2 * i;
+					}
+					// 下次改变方向
+					flag *= -1;
+				}
+			}
+			// 结束一行遍历，方向标志位还原
+			flag = 1;
+		}
+		return sb.toString();
 	}
 }
