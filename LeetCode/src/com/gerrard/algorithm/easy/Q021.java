a228d1dd21ce29c0ef0f55a2f871ecc4e33f3acd
Gerrard_Feng <kevin_gerrard@sina.com>
2016-10-18 13:31:43 +0800

    Commit Q021

--- LeetCode/src/com/gerrard/algorithm/easy/Q021.java
+++ LeetCode/src/com/gerrard/algorithm/easy/Q021.java
@@ -6,10 +6,14 @@ public class Q021 {
 		SinglyLinkedList<Integer> list1 = new SinglyLinkedList<>();
 		list1.add(1);
 		list1.add(3);
-		list1.add(4);
+		list1.add(3);
+		list1.add(6);
+		list1.add(8);
+		list1.add(9);
 		SinglyLinkedList<Integer> list2 = new SinglyLinkedList<>();
 		list2.add(2);
 		list2.add(5);
+		list2.add(7);
 		SinglyLinkedList<Integer> result = method(list1, list2);
 		for (int i = 0; i < result.size(); i++) {
 			System.out.println(result.get(i));
@@ -20,7 +24,6 @@ public class Q021 {
 			SinglyLinkedList<Integer> sortedList2) {
 		SinglyLinkedList<Integer> list = new SinglyLinkedList<>();
 		return list.sort(sortedList1, sortedList2);
-
 	}
 }
 
@@ -42,7 +45,7 @@ class SinglyLinkedList<E> {
 	Node<E> first;
 	Node<E> head = new Node<>(null, null);
 
-	public void add(E e) {
+	void add(E e) {
 		Node<E> newNode = new Node<>(e, null);
 		Node<E> preNode = last;
 		if (last == null) {
@@ -55,7 +58,7 @@ class SinglyLinkedList<E> {
 		size++;
 	}
 
-	public int get(int index) {
+	int get(int index) {
 		checkIndex(index);
 		Node<E> currentNode = first;
 		for (int i = 0; i < size; i++) {
@@ -73,6 +76,7 @@ class SinglyLinkedList<E> {
 		Node<E> currentNode = first;
 		for (int i = 0; i < size; i++) {
 			if (i < index) {
+				currentNode = currentNode.next;
 			} else {
 				break;
 			}
@@ -82,7 +86,7 @@ class SinglyLinkedList<E> {
 
 	private void checkIndex(int index) {
 		if (index > size - 1 || index < 0) {
-			String errorMessage = "size=" + size + ";index=" + "index.";
+			String errorMessage = "size=" + size + ";index=" + index;
 			throw new IndexOutOfBoundsException(errorMessage);
 		}
 	}
@@ -92,31 +96,45 @@ class SinglyLinkedList<E> {
 	}
 
 	// 对两个已经排序的链表进行排序，不消耗多余的Node空间
-	public SinglyLinkedList<E> sort(SinglyLinkedList<E> sortedList1, SinglyLinkedList<E> sortedList2) {
-		// 以sortedList1为基准排序
-		Node<E> lastNode = sortedList1.head;
-		// 赋值size
-		sortedList1.size = sortedList1.size + sortedList2.size;
-		// 断开的链表，初始赋值为链表2
-		SinglyLinkedList<E> tempList = sortedList2;
+	SinglyLinkedList<E> sort(SinglyLinkedList<E> sortedList, SinglyLinkedList<E> tempList) {
+		// 以sortedList为基准排序
+		Node<E> lastNode = sortedList.head;
 		// 开启循环
-		for (int i = 0; i < sortedList1.size; i++) {
+		for (int i = 0; i < sortedList.size; i++) {
 			if (tempList.size == 0) {
 				// tempList读完，break
 				// 之后的链表其实都是连接着的，已经有序
 				break;
 			}
 			// 比较
-			if (sortedList1.get(i) < tempList.get(0)) {
+			if (sortedList.get(i) < tempList.get(0)) {
 				// 不需要动tempList
-				lastNode.next = sortedList1.getNode(i);
+				lastNode.next = sortedList.getNode(i);
 			} else {
-				// 在断开链表的指针之前，优先赋值tempList，只要改变first就行了
-				// 如果不优先赋值，后面的Node指针断了，就找不回来了
-				tempList.first = lastNode.next;
-				lastNode.next = tempList.getNode(0);
+				// 先交换last
+				tempList.last = sortedList.getNode(sortedList.size - 1);
+				sortedList.last = tempList.getNode(tempList.size - 1);
+				// 注意断开链表的指针时，如果只有一个指针指向下个结点，一旦断开就找不回来了
+				// 重点在于tempList的第一个结点，有head.next，first，2个指针
+				tempList.head.next = lastNode.next;
+				lastNode.next = tempList.first;
+				tempList.first = tempList.head.next;
+				// 改变两个链表的size
+				int temp = sortedList.size;
+				sortedList.size = i + tempList.size;
+				tempList.size = temp - i;
+			}
+			// 更新lastNode
+			lastNode = sortedList.getNode(i);
+			// 当已排序链表到底了，证明temp里的都比sorted大，把指针连上就结束了
+			if (lastNode == sortedList.last) {
+				lastNode.next = tempList.first;
+				// 改变大小
+				sortedList.size += tempList.size;
+				// 注意退出
+				break;
 			}
 		}
-		return sortedList1;
+		return sortedList;
 	}
 }
