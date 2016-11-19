e1398e59071dcea1c43722e0387952263d427cd4
ZXW-20161112YIS\Administrator <kevin_gerrard@sina.com>
2016-11-19 15:19:17 +0800

    improve Q009

--- LeetCode/src/com/gerrard/algorithm/easy/Q009.java
+++ LeetCode/src/com/gerrard/algorithm/easy/Q009.java
@@ -3,34 +3,28 @@ package com.gerrard.algorithm.easy;
 public class Q009 {
 
 	public static void main(String[] args) {
-		System.out.println(method(0));
-		System.out.println(method(2));
-		System.out.println(method(34));
-		System.out.println(method(345543));
-		System.out.println(method(1246421));
+
+		System.out.println(isPalindrome(0));
+		System.out.println(isPalindrome(2));
+		System.out.println(isPalindrome(34));
+		System.out.println(isPalindrome(345543));
+		System.out.println(isPalindrome(1246421));
 	}
 
-	private static boolean method(int number) {
-		// 负数不在讨论范围
-		if (number < 0) {
+	public static boolean isPalindrome(int x) {
+		// 负数不在讨论范围内
+		if (x < 0) {
 			return false;
 		}
-		// 确定最高位（0的最高位数是0，等下特殊处理）
-		int maxLevel = 0;
-		int copyNumber = number;
-		while (copyNumber > 0) {
-			copyNumber /= 10;
-			maxLevel++;
-		}
-		// 低高位
+		int length = String.valueOf(x).length();
+		// 低位/高位
 		int low;
 		int high;
-		// 无论最高位是奇偶，遍历至maxLevel/2就行了
-		// maxLevel=0在这里直接跳过了
-		for (int i = 1; i < maxLevel / 2 + 1; i++) {
-			// 低位+高位的和是maxLevel+1
-			low = getIndexNumber(i, number);
-			high = getIndexNumber(maxLevel + 1 - i, number);
+		// 循环至一半就可以退出
+		for (int i = 1; i < length / 2 + 1; i++) {
+			// low+high=length+1
+			low = getNFromLow(i, x);
+			high = getNFromLow(length + 1 - i, x);
 			if (!(low == high)) {
 				return false;
 			}
@@ -38,9 +32,8 @@ public class Q009 {
 		return true;
 	}
 
-	// 获取一个数字具体某一位的数字
-	// index是自低位起的数字
-	private static int getIndexNumber(int index, int number) {
-		return (int) ((number / Math.pow(10, index - 1)) % 10);
+	// 获取数字x，自低位起的第n个数
+	private static int getNFromLow(int n, int x) {
+		return (int) ((x / Math.pow(10, n - 1)) % 10);
 	}
 }
