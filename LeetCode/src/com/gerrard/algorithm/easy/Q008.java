db4c04c007d2c43076fe4cb7a6742cb197dd6204
ZXW-20161112YIS\Administrator <kevin_gerrard@sina.com>
2016-11-19 14:42:49 +0800

    improve Q008

--- LeetCode/src/com/gerrard/algorithm/easy/Q008.java
+++ LeetCode/src/com/gerrard/algorithm/easy/Q008.java
@@ -3,64 +3,63 @@ package com.gerrard.algorithm.easy;
 public class Q008 {
 
 	public static void main(String[] args) {
-		System.out.println(method(" 23.45"));
-		System.out.println(method(" a23.45"));
-		System.out.println(method(" 2a3.45"));
-		System.out.println(method("   +23.45"));
-		System.out.println(method("   -23.45"));
-		System.out.println(method("   2345"));
-		System.out.println(method("123456789876"));
-		System.out.println(method("-123456789876"));
+		System.out.println(myAtoi(" 23.45"));
+		System.out.println(myAtoi(" a23.45"));
+		System.out.println(myAtoi(" 2a3.45"));
+		System.out.println(myAtoi("   +23.45"));
+		System.out.println(myAtoi("   -23.45"));
+		System.out.println(myAtoi("   2345 "));
+		System.out.println(myAtoi("123456789876"));
+		System.out.println(myAtoi("-123456789876"));
 	}
 
-	private static int method(String str) {
+	public static int myAtoi(String str) {
+		if (str == null) {
+			throw new IllegalArgumentException("Input error");
+		}
 		// 先去空格
 		char[] array = str.trim().toCharArray();
-		// 0特殊处理
+		// 长度为0的特殊处理
 		if (array.length == 0) {
 			return 0;
 		}
 		// 正负号标志位
-		int operatorFlag = 1;
-		// 记录返回数字，超过long表示范围不考虑
+		int sign = 1;
 		long sum = 0;
 		if (array[0] == '+' || array[0] == '-' || (array[0] >= '0' && array[0] <= '9')) {
-			// 操作符置0，便于统一处理
+			// 操作符置char型的'0'
 			if (array[0] == '+') {
-				// 这里赋值，注意是char型的
 				array[0] = '0';
-				operatorFlag = 1;
+				sign = 1;
 			} else if (array[0] == '-') {
 				array[0] = '0';
-				operatorFlag = -1;
+				sign = -1;
 			}
-			// 最高位
-			int maxLevel = 0;
-			// 循环确定最高位
+			// 循环确定长度
+			int length = 0;
 			for (int i = 0; i < array.length; i++) {
 				if (array[i] >= '0' && array[i] <= '9') {
-					maxLevel++;
+					length++;
 				} else {
-					// 遇到非数字退出
+					// 遇到非数值退出循环
 					break;
 				}
 			}
-			// 需要一个maxLevel的副本来赋值
-			int copyMaxLevel = maxLevel;
-			// 累加赋值，注意其实位置的下标
-			for (int i = 0; i < maxLevel; i++) {
-				// 注意char型的数字，转成数字的处理
-				sum += (array[i] - '0') * Math.pow(10, --copyMaxLevel);
+			// length值作为退出条件，但同时又要自减，准备一个副本
+			int exit = length;
+			for (int i = 0; i < exit; i++) {
+				// char型的数值，转换成数值型处理
+				sum += (array[i] - '0') * Math.pow(10, --length);
 			}
 			// 超出范围处理
-			long realNumber = sum * operatorFlag;
-			if (realNumber > Integer.MAX_VALUE) {
+			long num = sum * sign;
+			if (num > Integer.MAX_VALUE) {
 				return Integer.MAX_VALUE;
 			}
-			if (realNumber < Integer.MIN_VALUE) {
+			if (num < Integer.MIN_VALUE) {
 				return Integer.MIN_VALUE;
 			}
-			return (int) realNumber;
+			return (int) num;
 		} else {
 			return 0;
 		}
