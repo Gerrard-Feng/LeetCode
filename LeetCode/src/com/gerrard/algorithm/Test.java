c98d5cea4beef6f1863f2af1a9ea50ba640b25c7
MVBZ0QOH5TOJGMV\Administrator <whf11123768@163.com>
2016-11-21 17:36:17 +0800

    improve Q010

--- LeetCode/src/com/gerrard/algorithm/Test.java
+++ LeetCode/src/com/gerrard/algorithm/Test.java
@@ -1,11 +1,6 @@
 package com.gerrard.algorithm;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
-import java.util.LinkedHashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Set;
 
 public class Test {
 
--- LeetCode/src/com/gerrard/algorithm/hard/Q010.java
+++ LeetCode/src/com/gerrard/algorithm/hard/Q010.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public class Q010 {
 
-	// 只考虑正则表达式有"."和"*"的情况，其他符号不考虑，包括括号
+	// 只考虑正则表达式有"."和"*"的情况，其他符号不考虑
 	public static void main(String[] args) {
 		System.out.println(check("aa", "a"));
 		System.out.println(check("aa", "aa"));
@@ -31,6 +31,7 @@ public class Q010 {
 		System.out.println(check("aabcbcbcaccbcaabc", "a*aa*.*b*.c*.*a*"));
 		System.out.println(check("cbbbaababbac", ".*c*."));
 		System.out.println(check("a", "a*aa*"));
+		System.out.println(check("bbabcaccacbcacaa", ".*.c*a*.c"));
 	}
 
 	// 校验匹配的准确性
@@ -41,18 +42,31 @@ public class Q010 {
 	}
 
 	public static boolean isMatch(String s, String p) {
-		// 递归方法时不适合使用入参检查的
+		// 递归方法不适用入参检查
+		if (s == null || p == null) {
+			throw new IllegalArgumentException("Input error");
+		}
+		// 最后一位匹配失败，直接匹配失败
+		if (p.length() > 0 && s.length() > 0 && !p.substring(p.length() - 1).equals("*")
+				&& !singleMatch(s.charAt(s.length() - 1), p.charAt(p.length() - 1))) {
+			return false;
+		}
+		return isMatchTrue(s, p);
+	}
+
+	private static boolean isMatchTrue(String s, String p) {
 		// 待处理的正则
 		String pDeal;
 		// 消去的字符串长度
 		int sReduce;
+		// 以字符串为主体，匹配正则
 		while (s.length() > 0) {
 			// 正则长度为0
 			if (p.length() == 0) {
 				return false;
 			}
 			if (p.length() > 1 && p.charAt(1) == '*') {
-				// 第二个数是"*"情况
+				// 第二个字符：*
 				pDeal = p.substring(0, 2);
 				sReduce = starMatch(s, p);
 				// 在内部方法中，已经通过递归得出结果
@@ -62,7 +76,7 @@ public class Q010 {
 					return false;
 				}
 			} else {
-				// 单个匹配情况
+				// 单字符匹配
 				pDeal = p.substring(0, 1);
 				if (!singleMatch(s.charAt(0), p.charAt(0))) {
 					return false;
@@ -73,26 +87,25 @@ public class Q010 {
 			s = s.substring(sReduce);
 			p = p.substring(pDeal.length());
 		}
-		// 待解析完成，但正则还有
+		// 字符串解析完成，但正则还有剩余
 		if (!regularEqualsNull(p)) {
 			return false;
 		}
 		return true;
 	}
 
-	// 普通字符+"*"，返回"*"匹配的长度
-	private static int starMatchWithoutPoint(String s, String p) {
-		// "*"之前的字符
+	// 普通字符+*，返回*匹配的长度
+	private static int starMatchNormal(String s, String p) {
 		char pBeforeStar = p.charAt(0);
-		// 正则长度是2的情况
+		// 正则长度：2
 		if (p.length() == 2) {
 			return getLength(s, pBeforeStar);
 		}
 		char pAfterStar = p.charAt(2);
-		// 正则长度是3的情况
+		// 正则长度：3
 		if (p.length() == 3) {
 			int l = getLength(s, pBeforeStar);
-			// 根据s的最后一个字符，改变返回长度
+			// 字符串s的最后一个字符，会影响*匹配长度
 			if (singleMatch(s.charAt(s.length() - 1), pAfterStar)) {
 				if (singleMatch(pBeforeStar, pAfterStar)) {
 					l--;
@@ -103,52 +116,47 @@ public class Q010 {
 			}
 			return l < 0 ? 0 : l;
 		}
-		// 正则第四个为值是*
+		// 正则第四个字符：*
 		if (p.charAt(3) == '*') {
-			// .*匹配所有情况，所以前两个正则可以尽可能多的消去
+			// 如(aaabcd,a*.*d)
+			// a*是否存在，不影响整体的匹配结果
+			// 但是可以尽可能消去字符串s中，a起始的个数，减小.*匹配的负担
 			if (pAfterStar == '.') {
 				return getLength(s, pBeforeStar);
 			}
-			// 类似a*a*等价于a*，递归
+			// 如a*a*，与a*等价
 			if (pAfterStar == pBeforeStar) {
-				return starMatchWithoutPoint(s, p.substring(2));
+				return isMatchTrue(s, p.substring(2)) ? -1 : -2;
 			}
-			// 余下情况，类似于a*b*，先考虑b*匹配长度
-			// 顺序不能反，后者有null的风险
-			// 触发自动装箱
-			Character c = pAfterStar;
-			if (!c.equals(notXFromStart(s, pBeforeStar))) {
-				// 去掉b*，递归
-				return isMatch(s, p.substring(0, 2) + p.substring(4)) ? -1 : -2;
+			// 余下情况，如a*b*，考虑b*匹配长度
+			if (pAfterStar != notXFromStart(s, pBeforeStar)) {
+				// b*匹配长度：0
+				return isMatchTrue(s, p.substring(0, 2) + p.substring(4)) ? -1 : -2;
 			}
-			// b*有匹配，可以确定a*的匹配长度
+			// b*匹配长度大于1；或字符串全部由a组成
 			return getLength(s, pBeforeStar);
 		} else {
-			// 类似a*.情况
+			// 如a*.
+			// 无法确定*匹配的具体长度
 			if (p.charAt(2) == '.') {
 				// a*之后，所有的正则
 				String pAfterPoint = p.substring(2);
-				// -1代表成功，-2代表失败
-				// a*匹配0个
-				if (isMatch(s, pAfterPoint)) {
-					return -1;
-				}
-				// a*匹配1个，至字符串s前面所有的a
-				for (int i = 0; i < s.length(); i++) {
-					if (s.charAt(i) == pBeforeStar) {
-						// 字符串去掉第一个a
-						s = s.substring(1);
-						if (isMatch(s, pAfterPoint)) {
-							return -1;
-						}
-					} else {
+				// 匹配字符串中，*所有可能性
+				int posibility = getLength(s, pBeforeStar) + 1;
+				for (int i = 0; i < posibility; i++) {
+					if (isMatchTrue(s, pAfterPoint)) {
+						return -1;
+					}
+					if (s.length() == 0) {
 						return -2;
 					}
+					s = s.substring(1);
 				}
+				return -2;
 			}
-			// 类似a*a的情况
+			// 如a*a
 			if (p.charAt(2) == pBeforeStar) {
-				// 计算a*之后a的个数，已经至少有1个
+				// a*之后a的个数
 				int count = 1;
 				for (int i = 3; i < p.length(); i++) {
 					if (p.charAt(i) == pBeforeStar) {
@@ -157,7 +165,7 @@ public class Q010 {
 						break;
 					}
 				}
-				// 类似a*aaaab的b
+				// 如a*aaaab的b
 				Character after = count == p.length() - 2 ? null : p.charAt(count + 2);
 				int l = getLength(s, pBeforeStar);
 				if (after == null || !(after.equals('.') || after.equals('*'))) {
@@ -167,34 +175,35 @@ public class Q010 {
 					}
 					return l - count;
 				}
-				// 类似a*aaa.b匹配aaaacb，count=3
+				// 如(a*aaa.b,aaaacb)，count=3
 				if (after.equals('.')) {
 					if (count > l) {
 						return -2;
 					}
-					// 等价于a*.b匹配acb
+					// 等价(a*.b,acb)
 					s = s.substring(count);
 					p = p.substring(0, 2) + p.substring(2 + count);
-					if (isMatch(s, p)) {
+					if (isMatchTrue(s, p)) {
 						return -1;
 					}
 					return -2;
 				}
+				// 如(a*aaa*b,aaaacb)，count=2
+				// 等价(a*b,aacb)
 				if (after.equals('*')) {
-					// 最后一个a匹配*，不作数
 					count--;
 					if (count > l) {
 						return -2;
 					}
 					s = s.substring(count);
-					p = p.substring(0, 2) + p.substring(2 + count);
-					if (isMatch(s, p)) {
+					p = p.substring(2 + count);
+					if (isMatchTrue(s, p)) {
 						return -1;
 					}
 					return -2;
 				}
 			}
-			// 剩余情况类似a*ba
+			// 余下情况，如a*ba
 			return getLength(s, pBeforeStar);
 		}
 	}
@@ -202,25 +211,23 @@ public class Q010 {
 	// 返回*匹配长度
 	private static int starMatch(String s, String p) {
 		if (p.charAt(0) == '.') {
-			// .*处理，先把对应的正则字符串去掉.*
+			// .*
 			p = p.substring(2);
-			// .*之后的正则，可以匹配空字符串，直接匹配成功
+			// .*之后的正则，如果可以匹配空字符串，直接匹配成功
 			if (regularEqualsNull(p)) {
 				return -1;
 			}
 			// 用余下的正则，循环递归
 			for (int i = 0; i < s.length(); i++) {
-				// 开始递归
-				String sWithoutHead = s.substring(i);
-				if (isMatch(sWithoutHead, p)) {
-					// 只要出现true，直接整个都可以匹配
+				String sAfter = s.substring(i);
+				if (isMatchTrue(sAfter, p)) {
 					return -1;
 				}
 			}
 			// 余下的都不成功，表示整个不匹配
 			return -2;
 		} else {
-			return starMatchWithoutPoint(s, p);
+			return starMatchNormal(s, p);
 		}
 	}
 
@@ -247,14 +254,14 @@ public class Q010 {
 	}
 
 	// 在字符串s中，第一个不是x的字符
-	private static Character notXFromStart(String s, char x) {
+	private static char notXFromStart(String s, char x) {
 		for (int i = 0; i < s.length(); i++) {
 			if (s.charAt(i) != x) {
 				return s.charAt(i);
 			}
 		}
 		// s全部由x组成
-		return null;
+		return x;
 	}
 
 	// 字符串s中，以pBeforeStar开头的个数
