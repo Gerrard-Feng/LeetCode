1147b313b0d33eefa4f5c6e9155d0eaabf08e264
ZXW-20161112YIS\Administrator <kevin_gerrard@sina.com>
2016-11-22 00:40:40 +0800

    improve Q011

--- LeetCode/src/com/gerrard/algorithm/medium/Q011.java
+++ LeetCode/src/com/gerrard/algorithm/medium/Q011.java
@@ -3,36 +3,64 @@ package com.gerrard.algorithm.medium;
 public class Q011 {
 
 	public static void main(String[] args) {
-		int[] array1 = new int[] { 2, 7, 3, 11, 8 };
-		int[] result1 = method(array1);
-		int index1 = result1[0];
-		int index2 = result1[1];
-		int height1 = Math.min(array1[index1], array1[index2]);
-		int length1 = Math.abs(index1 - index2);
-		System.out.println("索引值：" + index1 + "," + index2);
-		System.out.println("面积：" + height1 * length1);
+		int[] height = new int[15000];
+		for (int i = 15000; i > 0; i--) {
+			height[15000 - i] = i;
+		}
+		System.out.println(method_1(height));
+		System.out.println(method_2(height));
+		System.out.println(maxArea(height));
 	}
 
-	private static int[] method(int[] array) {
-		int[] result = new int[2];
-		int maxArea = 0;
-		for (int i = 0; i < array.length; i++) {
+	public static int maxArea(int[] height) {
+		if (height == null || height.length < 2) {
+			throw new IllegalArgumentException("Input error");
+		}
+		int l = 0, r = height.length - 1;
+        int area = (r - l) * Math.min(height[l],height[r]);
+        while (l < r) {
+            if (height[l] < height[r]) {
+                l++;
+                area = Math.max(area, (r - l) * Math.min(height[l],height[r]));
+            }
+            else {
+                r--;
+                area = Math.max(area, (r - l) * Math.min(height[l],height[r]));
+            }
+        }
+        return area;
+	}
+
+	private static int method_2(int[] height) {
+		int max = 0;
+		for (int i = 0; i < height.length; i++) {
 			int maxHeight = 0;
-			// 第二次反向遍历
-			for (int j = array.length - 1; j > i; j--) {
-				int height = Math.min(array[i], array[j]);
+			// 内循环反向遍历
+			for (int j = height.length - 1; j > i; j--) {
+				int h = Math.min(height[i], height[j]);
 				// 因为底越来越小，所以只有高度大于最高高度，才有比较面积的意义
-				if (height > maxHeight) {
+				if (h > maxHeight) {
 					// 不考虑面积比较结果，高先赋值
-					maxHeight = height;
-					if (height * (j - i) > maxArea) {
-						maxArea = height * (j - i);
-						result[0] = i;
-						result[1] = j;
+					maxHeight = h;
+					if (h * (j - i) > max) {
+						max = h * (j - i);
 					}
 				}
 			}
 		}
-		return result;
+		return max;
+	}
+
+	private static int method_1(int[] height) {
+		int max = 0;
+		for (int i = 0; i < height.length - 1; i++) {
+			for (int j = i + 1; j < height.length; j++) {
+				int area = Math.min(height[i], height[j]) * (j - i);
+				if (max < area) {
+					max = area;
+				}
+			}
+		}
+		return max;
 	}
 }
