3a1719118da0e093150123b7ddb8f917a534042e
MVBZ0QOH5TOJGMV\Administrator <whf11123768@163.com>
2016-11-22 15:00:49 +0800

    improve Q011

--- LeetCode/src/com/gerrard/algorithm/medium/Q011.java
+++ LeetCode/src/com/gerrard/algorithm/medium/Q011.java
@@ -16,19 +16,47 @@ public class Q011 {
 		if (height == null || height.length < 2) {
 			throw new IllegalArgumentException("Input error");
 		}
-		int l = 0, r = height.length - 1;
-        int area = (r - l) * Math.min(height[l],height[r]);
-        while (l < r) {
-            if (height[l] < height[r]) {
-                l++;
-                area = Math.max(area, (r - l) * Math.min(height[l],height[r]));
-            }
-            else {
-                r--;
-                area = Math.max(area, (r - l) * Math.min(height[l],height[r]));
-            }
-        }
-        return area;
+		// 初始面积
+		int left = 0, right = height.length - 1;
+		int area = (right - left) * Math.min(height[left], height[right]);
+		// 左右侧开始的最长高度
+		int leftMax = height[left];
+		int rightMax = height[right];
+		// 从两侧向中间夹逼，即底在不断变小
+		while (left < right) {
+			if (height[left] < height[right]) {
+				left++;
+				// 更小的高没有比较价值
+				if (height[left] <= leftMax) {
+					continue;
+				} else {
+					leftMax = height[left];
+				}
+				area = Math.max(area, (right - left) * Math.min(height[left], height[right]));
+			} else {
+				right--;
+				if (height[right] <= rightMax) {
+					continue;
+				} else {
+					rightMax = height[right];
+				}
+				area = Math.max(area, (right - left) * Math.min(height[left], height[right]));
+			}
+		}
+		return area;
+	}
+
+	private static int method_1(int[] height) {
+		int max = 0;
+		for (int i = 0; i < height.length - 1; i++) {
+			for (int j = i + 1; j < height.length; j++) {
+				int area = Math.min(height[i], height[j]) * (j - i);
+				if (max < area) {
+					max = area;
+				}
+			}
+		}
+		return max;
 	}
 
 	private static int method_2(int[] height) {
@@ -45,19 +73,6 @@ public class Q011 {
 					if (h * (j - i) > max) {
 						max = h * (j - i);
 					}
-				}
-			}
-		}
-		return max;
-	}
-
-	private static int method_1(int[] height) {
-		int max = 0;
-		for (int i = 0; i < height.length - 1; i++) {
-			for (int j = i + 1; j < height.length; j++) {
-				int area = Math.min(height[i], height[j]) * (j - i);
-				if (max < area) {
-					max = area;
 				}
 			}
 		}
