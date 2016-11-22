97152364b56cf6f1351c8d4802a21e962073892c
ZXW-20161112YIS\Administrator <kevin_gerrard@sina.com>
2016-11-22 23:47:52 +0800

    improve Q014

--- LeetCode/src/com/gerrard/algorithm/easy/Q014.java
+++ LeetCode/src/com/gerrard/algorithm/easy/Q014.java
@@ -3,49 +3,71 @@ package com.gerrard.algorithm.easy;
 public class Q014 {
 
 	public static void main(String[] args) {
+
+		System.out.println("<==========第一组测试==========>");
 		String[] array1 = new String[] { "abcds", "abdee", "abcww", "ab" };
-		String[] array2 = new String[] { "abcds", "abdee", "", "ab" };
-		String[] array3 = new String[] { "abcds", "abdee", "d", "ab" };
-		String[] array4 = new String[] { "abcds", "abdee", "a", "ar" };
+		System.out.println(longestCommonPrefix(array1));
 		System.out.println(method(array1));
+		System.out.println();
+
+		System.out.println("<==========第二组测试==========>");
+		String[] array2 = new String[] { "aaa", "aa", "aaa" };
+		System.out.println(longestCommonPrefix(array2));
 		System.out.println(method(array2));
+		System.out.println();
+
+		System.out.println("<==========第三组测试==========>");
+		String[] array3 = new String[] { "", "b" };
+		System.out.println(longestCommonPrefix(array3));
 		System.out.println(method(array3));
+		System.out.println();
+
+		System.out.println("<==========第四组测试==========>");
+		String[] array4 = new String[] { "aa", "a" };
+		System.out.println(longestCommonPrefix(array4));
 		System.out.println(method(array4));
 	}
 
-	private static String method(String[] array) {
-		// 特殊处理
-		if (array == null || array.length == 0) {
-			return "";
-		} else if (array.length == 1) {
-			return array[0];
+	public static String longestCommonPrefix(String[] strs) {
+		if (strs == null) {
+			throw new IllegalArgumentException("Input error");
 		}
-		// 正常情况
-		String currentPrefix = array[0];
-		// 从数组第二项开始遍历
-		for (int i = 1; i < array.length; i++) {
-			// 比较长度取较小者
-			int length = Math.min(currentPrefix.length(), array[i].length());
-			// 出现长度为0的空字符串，不作处理，直接返回
-			if (length == 0) {
-				return "";
+		String prefix = strs.length == 0 ? "" : strs[0];
+		for (String compare : strs) {
+			while (!compare.startsWith(prefix)) {
+				// 不匹配，消去前缀最后一项
+				prefix = prefix.substring(0, prefix.length() - 1);
 			}
-			StringBuffer sb = new StringBuffer();
-			for (int j = 0; j < length; j++) {
-				// 记得用String.equals()方法
-				if (currentPrefix.substring(j, j + 1).equals(array[i].substring(j, j + 1))) {
-					sb.append(currentPrefix.substring(j, j + 1));
-				} else {
-					// 遇到不一样，赋值退出
-					currentPrefix = sb.toString();
-					// 匹配长度为0，直接返回
-					if (currentPrefix.length() == 0) {
-						return "";
-					}
+			if (prefix.length() == 0) {
+				break;
+			}
+		}
+		return prefix;
+	}
+
+	private static String method(String[] strs) {
+		String prefix = strs.length == 0 ? "" : strs[0];
+		// 从数组第二项开始遍历
+		for (String compare : strs) {
+			// 整理长度
+			String s1 = prefix.length() > compare.length() ? compare : prefix;
+			String s2 = prefix.length() > compare.length() ? prefix : compare;
+			boolean flag = false;
+			for (int j = 0; j < s1.length(); j++) {
+				if (s1.charAt(j) != s2.charAt(j)) {
+					prefix = prefix.substring(0, j);
+					flag = true;
 					break;
 				}
 			}
+			// 防止完全匹配的情况
+			if (!flag) {
+				prefix = s1;
+			}
+			if (prefix.length() == 0) {
+				break;
+			}
 		}
-		return currentPrefix;
+		return prefix;
 	}
 }
