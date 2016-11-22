bcd0a7ae09a766d3568e2b82aa95ac5f6483eb58
MVBZ0QOH5TOJGMV\Administrator <whf11123768@163.com>
2016-11-22 17:48:58 +0800

    improve Q012

--- LeetCode/src/com/gerrard/algorithm/medium/Q012.java
+++ LeetCode/src/com/gerrard/algorithm/medium/Q012.java
@@ -3,45 +3,90 @@ package com.gerrard.algorithm.medium;
 public class Q012 {
 
 	public static void main(String[] args) {
+
+		System.out.println("<==========第一组测试==========>");
+		System.out.println(intToRoman(2094));
 		System.out.println(method(2094));
+		System.out.println();
+
+		System.out.println("<==========第二组测试==========>");
+		System.out.println(intToRoman(1234));
 		System.out.println(method(1234));
+		System.out.println();
+
+		System.out.println("<==========第三组测试==========>");
+		System.out.println(intToRoman(3000));
 		System.out.println(method(3000));
+		System.out.println();
+
+		System.out.println("<==========第四组测试==========>");
+		System.out.println(intToRoman(39));
 		System.out.println(method(39));
+		System.out.println();
+
+		System.out.println("<==========第五组测试==========>");
+		System.out.println(intToRoman(654));
 		System.out.println(method(654));
 	}
 
-	private static String method(int number) {
-		// 入参保护
-		if (number > 3999 || number < 1) {
-			return null;
+	public static String intToRoman(int num) {
+		if (num > 3999 || num < 1) {
+			throw new IllegalArgumentException("Input error");
 		}
-		// 结果集
 		StringBuffer result = new StringBuffer();
-		// 罗马字符集
-		char[][] romanArray = new char[][] { { 'I', 'V' }, { 'X', 'L' }, { 'C', 'D' }, { 'M' } };
-		// 创建副本确定最高位数
-		int maxLevel = 0;
-		int copyNumber = number;
-		while (copyNumber > 0) {
-			copyNumber /= 10;
-			maxLevel++;
-		}
-		while (--maxLevel >= 0) {
+		// 确定最高位数
+		int length = String.valueOf(num).length();
+		while (length-- > 0) {
 			// 罗马数字从左往右是最高位的
-			int currentNumber = (int) ((number / Math.pow(10, maxLevel)) % 10);
-			// 4,9特别处理
-			if (currentNumber == 4) {
-				result.append("" + romanArray[maxLevel][0] + romanArray[maxLevel][1]);
-			} else if (currentNumber == 9) {
-				result.append("" + romanArray[maxLevel][0] + romanArray[maxLevel + 1][0]);
+			int current = (int) ((num / Math.pow(10, length)) % 10);
+			// 4、9特殊处理
+			if (current == 4) {
+				result.append("" + romanDict(length, 0) + romanDict(length, 1));
+			} else if (current == 9) {
+				result.append("" + romanDict(length, 0) + romanDict(length + 1, 0));
 			} else {
 				// 大于等于5处理
-				if (currentNumber / 5 == 1) {
-					result.append(romanArray[maxLevel][1]);
+				if (current / 5 == 1) {
+					result.append(romanDict(length, 1));
 				}
 				// 加1
-				for (int i = 0; i < currentNumber % 5; i++) {
-					result.append(romanArray[maxLevel][0]);
+				for (int i = 0; i < current % 5; i++) {
+					result.append(romanDict(length, 0));
+				}
+			}
+		}
+		return result.toString();
+	}
+
+	// 罗马数字字典
+	private static char romanDict(int x, int y) {
+		char[] roman = new char[] { 'I', 'V', 'X', 'L', 'C', 'D', 'M' };
+		return roman[x * 2 + y];
+	}
+
+	// 使用二维数组，存储罗马字符集
+	private static String method(int num) {
+		StringBuffer result = new StringBuffer();
+		// 罗马字符集
+		char[][] roman = new char[][] { { 'I', 'V' }, { 'X', 'L' }, { 'C', 'D' }, { 'M' } };
+		// 确定最高位数
+		int length = String.valueOf(num).length();
+		while (length-- > 0) {
+			// 罗马数字从左往右是最高位的
+			int current = (int) ((num / Math.pow(10, length)) % 10);
+			// 4、9特殊处理
+			if (current == 4) {
+				result.append("" + roman[length][0] + roman[length][1]);
+			} else if (current == 9) {
+				result.append("" + roman[length][0] + roman[length + 1][0]);
+			} else {
+				// 大于等于5处理
+				if (current / 5 == 1) {
+					result.append(roman[length][1]);
+				}
+				// 加1
+				for (int i = 0; i < current % 5; i++) {
+					result.append(roman[length][0]);
 				}
 			}
 		}
