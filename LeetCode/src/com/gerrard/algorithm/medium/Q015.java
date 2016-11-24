ddc1d3b666fb5b354522f41dc448a5cbab35fdbc
MVBZ0QOH5TOJGMV\Administrator <whf11123768@163.com>
2016-11-24 10:41:34 +0800

    improve Q015

--- LeetCode/src/com/gerrard/algorithm/medium/Q015.java
+++ LeetCode/src/com/gerrard/algorithm/medium/Q015.java
@@ -58,9 +58,6 @@ public class Q015 {
 		Arrays.sort(nums);
 		int previous = 1;
 		for (int i = 0; i < nums.length - 2; i++) {
-			if (nums[i] > 0) {
-				return result;
-			}
 			if (nums[i] == previous) {
 				continue;
 			} else {
