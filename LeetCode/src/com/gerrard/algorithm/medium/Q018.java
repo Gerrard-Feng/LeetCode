36c756d96a1a60586235b3561fb2f23c38d4b757
MVBZ0QOH5TOJGMV\Administrator <whf11123768@163.com>
2016-11-24 20:17:26 +0800

    improve Q018

--- LeetCode/src/com/gerrard/algorithm/medium/Q018.java
+++ LeetCode/src/com/gerrard/algorithm/medium/Q018.java
@@ -1,19 +1,19 @@
 package com.gerrard.algorithm.medium;
 
 import java.util.Arrays;
-import java.util.LinkedHashSet;
-import java.util.Set;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Q018 {
 
 	public static void main(String[] args) {
-		int[] array = new int[] { 1, 0, -1, 0, -2, 2, 3, 4, -7, -7 };
-		Set<int[]> set = method(array, 0, 4);
-		for (int[] a : set) {
+		int[] array = new int[] { -1, 0, -2, -2, };
+		List<List<Integer>> list = fourSum(array, -5);
+		for (List<Integer> a : list) {
 			System.out.print("[");
-			for (int i = 0; i < a.length; i++) {
-				System.out.print(a[i]);
-				if (i != a.length - 1) {
+			for (int i = 0; i < a.size(); i++) {
+				System.out.print(a.get(i));
+				if (i != a.size() - 1) {
 					System.out.print(",");
 				}
 			}
@@ -21,70 +21,72 @@ public class Q018 {
 		}
 	}
 
-	private static Set<int[]> method(int[] array, int target, int kSum) {
-		// 入参保护
-		if (array == null || array.length < kSum) {
-			return null;
+	public static List<List<Integer>> fourSum(int[] nums, int target) {
+		if (nums == null || nums.length < 4) {
+			throw new IllegalArgumentException("Input error");
 		}
-		// 第一次进来做一次排序，之后自然有序，无需排序
-		if (kSum == 4) {
-			Arrays.sort(array);
-		}
-		// 结果集
-		Set<int[]> result = new LinkedHashSet<>();
+		Arrays.sort(nums);
+		return kSum(nums, 0, target, 4);
+	}
+
+	private static List<List<Integer>> kSum(int[] nums, int startIndex, int target, int kSum) {
+		List<List<Integer>> result = new LinkedList<>();
 		// 递归终点是2Sum问题
 		if (kSum == 2) {
-			if (target < array[0]) {
-				// 因为数组有序，target如果小于数组第一项，返回空集合
-				return result;
-			} else {
-				int lastNumber = Integer.MAX_VALUE;
-				for (int i = 0; i < array.length - 1; i++) {
-					if (array[i] == lastNumber) {
-						continue;
-					}
-					int tempTarget = target - array[i];
-					// 秉承始终向后找的思想，下一项小于tempTarget，且下一项大于0，直接退出
-					// 而且tempTarget会越来越小，array[i+1]越来越大，再往后更加不可能了，直接返回
-					if (tempTarget < array[i + 1] && array[i + 1] > 0) {
-						return result;
-					}
-					int[] remainArray = Arrays.copyOfRange(array, i + 1, array.length);
-					int index = Arrays.binarySearch(remainArray, tempTarget);
-					if (index >= 0) {
-						int[] newArray = new int[2];
-						newArray[0] = array[i];
-						newArray[1] = remainArray[index];
-						result.add(newArray);
-
-					}
-					lastNumber = array[i];
+			int left = startIndex, right = nums.length - 1;
+			int preLeft = Integer.MIN_VALUE, preRight = Integer.MIN_VALUE;
+			while (left < right) {
+				if (nums[left] > target && nums[left] > 0) {
+					return result;
+				}
+				if (2 * nums[left] > target || 2 * nums[right] < target) {
+					return result;
+				}
+				if (nums[left] == preLeft) {
+					left++;
+					continue;
+				}
+				if (nums[right] == preRight) {
+					right--;
+					continue;
+				}
+				int sum = nums[left] + nums[right];
+				if (sum == target) {
+					List<Integer> list = new LinkedList<>();
+					list.add(nums[left]);
+					list.add(nums[right]);
+					result.add(list);
+				}
+				if (sum < target) {
+					preLeft = nums[left];
+					left++;
+				} else {
+					preRight = nums[right];
+					right--;
 				}
 			}
 		} else {
 			// 大于2Sum问题，使用递归
-			// 记录上一个值，这是必须的，因为Set集合对于2个一模一样的数组无能为力
-			int lastNumber = Integer.MAX_VALUE;
-			for (int i = 0; i < array.length - (kSum - 1); i++) {
-				if (array[i] == lastNumber) {
-					continue;
-				}
-				int tempTarget = target - array[i];
-				if (array[i + 1] > tempTarget && array[i + 1] > 0) {
+			int previous = Integer.MAX_VALUE;
+			for (int i = startIndex; i < nums.length - 1; i++) {
+				if (nums[i] > target && nums[i] > 0) {
 					return result;
 				}
-				// 开启递归
-				Set<int[]> tempResult = method(Arrays.copyOfRange(array, i + 1, array.length), tempTarget, kSum - 1);
-				for (int[] a : tempResult) {
-					int[] newArray = new int[a.length + 1];
-					newArray[0] = array[i];
-					for (int j = 0; j < a.length; j++) {
-						newArray[j + 1] = a[j];
-					}
-					result.add(newArray);
+				// target值的范围超过k个极值
+				if (kSum * nums[i] > target || kSum * nums[nums.length - 1] < target) {
+					return result;
 				}
-				// 下次循环前，变更lastNumber
-				lastNumber = array[i];
+				if (nums[i] == previous) {
+					continue;
+				}
+				int tempTarget = target - nums[i];
+				// 开启递归
+				List<List<Integer>> tempResult = kSum(nums, i + 1, tempTarget, kSum - 1);
+				for (List<Integer> a : tempResult) {
+					a.add(nums[i]);
+					result.add(a);
+				}
+				previous = nums[i];
 			}
 		}
 		return result;
