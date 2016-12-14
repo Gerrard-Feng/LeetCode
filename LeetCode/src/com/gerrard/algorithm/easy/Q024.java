c1ebae77fd71e71c2fa552df8d17af7cc9ce5943
MVBZ0QOH5TOJGMV\Administrator <whf11123768@163.com>
2016-12-14 10:45:41 +0800

    commit Q024: add swap nodes instead of swap value

--- LeetCode/src/com/gerrard/algorithm/easy/Q024.java
+++ LeetCode/src/com/gerrard/algorithm/easy/Q024.java
@@ -2,8 +2,40 @@ package com.gerrard.algorithm.easy;
 
 public class Q024 {
 
-	// 交换结点存放的数据
+	public static void main(String[] args) {
+
+		ListNode l1 = new ListNode(1);
+		l1.next = new ListNode(2);
+		l1.next.next = new ListNode(3);
+		ListNode head = swapPairs(l1);
+		while (head != null) {
+			System.out.print(head.val + " ");
+			head = head.next;
+		}
+	}
+
+	// 交换结点
 	public static ListNode swapPairs(ListNode head) {
+		// 第一个节点会变
+		ListNode first = head == null || head.next == null ? head : head.next;
+		// 上一个节点
+		ListNode last = new ListNode(0);
+		while (head != null && head.next != null) {
+			// 下一个节点
+			ListNode after = head.next;
+			// 交换节点
+			last.next = after;
+			head.next = after.next;
+			after.next = head;
+			// 下一次交换赋值
+			last = head;
+			head = head.next;
+		}
+		return first;
+	}
+
+	// 交换结点存放的数据
+	public static ListNode swapPairsVal(ListNode head) {
 		ListNode first = head;
 		while (head != null && head.next != null) {
 			// 交换数据，一步到位，但有溢出的风险
@@ -17,7 +49,6 @@ public class Q024 {
 		int val;
 		ListNode next;
 
-		@SuppressWarnings("unused")
 		ListNode(int x) {
 			val = x;
 		}
