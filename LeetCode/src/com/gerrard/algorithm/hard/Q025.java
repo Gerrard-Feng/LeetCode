3ac0db0f14da2352c7344fbcf02d40fc28a0a1fd
MVBZ0QOH5TOJGMV\Administrator <whf11123768@163.com>
2016-12-14 13:47:32 +0800

    improve Q025

--- LeetCode/src/com/gerrard/algorithm/hard/Q025.java
+++ LeetCode/src/com/gerrard/algorithm/hard/Q025.java
@@ -9,15 +9,56 @@ public class Q025 {
 		l1.next.next = new ListNode(3);
 		l1.next.next.next = new ListNode(4);
 		l1.next.next.next.next = new ListNode(5);
-		reverseKGroup(l1, 3);
-		ListNode check = l1;
+		ListNode check = reverseKGroup(l1, 3);
 		while (check != null) {
 			System.out.print(check.val + " ");
 			check = check.next;
 		}
 	}
 
+	// 交换节点
 	public static ListNode reverseKGroup(ListNode head, int k) {
+		if (head == null || k < 2) {
+			return head;
+		}
+		// 获取链表的长度
+		ListNode check = head;
+		int size = 0;
+		while (check != null) {
+			size++;
+			check = check.next;
+		}
+		// 确定返回的头节点
+		ListNode first = head;
+		if (k <= size) {
+			for (int i = 1; i < k; i++) {
+				first = first.next;
+			}
+		}
+		// 当前节点和上一个节点
+		ListNode current = head;
+		ListNode last = new ListNode(0);
+		ListNode[] reverse = new ListNode[k];
+		while (size > k - 1) {
+			for (int i = 0; i < k; i++) {
+				reverse[i] = current;
+				current = current.next;
+			}
+			// 倒序赋值
+			last.next = reverse[k - 1];
+			for (int i = k - 1; i > 0; i--) {
+				reverse[i].next = reverse[i - 1];
+			}
+			reverse[0].next = current;
+			// 更新上一个节点
+			last = reverse[0];
+			size -= k;
+		}
+		return first;
+	}
+
+	// 交换数据
+	public static ListNode reverseKGroupVal(ListNode head, int k) {
 		if (head == null || k < 2) {
 			return head;
 		}
