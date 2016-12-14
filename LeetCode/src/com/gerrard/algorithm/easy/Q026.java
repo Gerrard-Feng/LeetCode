a533ad99ac37547054a4b69bd1d1d84d48bc358f
MVBZ0QOH5TOJGMV\Administrator <whf11123768@163.com>
2016-12-14 09:47:32 +0800

    commit Q026

--- LeetCode/src/com/gerrard/algorithm/easy/Q026.java
+++ LeetCode/src/com/gerrard/algorithm/easy/Q026.java
@@ -12,99 +12,41 @@ public class Q026 {
 		for (int i = 0; i < newLength1; i++) {
 			System.out.print(array1[i] + " ");
 		}
-		System.out.println();
+		System.out.println("\n");
 
-		System.out.println("<==========第一组测试==========>");
+		System.out.println("<==========第二组测试==========>");
 		int[] array2 = new int[] { 0, 1, 1, 2, 2, 3, 3, 3, 4, 5, 6, 7, 7, 8, 8, 9, 9, 9, 9 };
 		int newLength2 = removeDuplicates(array2);
 		for (int i = 0; i < newLength2; i++) {
 			System.out.print(array2[i] + " ");
 		}
-		System.out.println();
+		System.out.println("\n");
 
 		System.out.println("<==========第三组测试==========>");
 		int[] array3 = new int[] { 1, 2, 3, 4, 4, 4, 5, 5, 6, 6, 6, 7, 8, 9 };
-		array3 = removeDuplicates_3(array3);
-		for (int i = 0; i < array3.length; i++) {
+		int newLength3 = removeDuplicates(array3);
+		for (int i = 0; i < newLength3; i++) {
 			System.out.print(array3[i] + " ");
 		}
 	}
 
 	public static int removeDuplicates(int[] array) {
 		if (array == null) {
-			return -1;
+			throw new IllegalArgumentException("Input error");
 		}
 		if (array.length < 2) {
 			return array.length;
 		}
-		// “新数组”的指针
+		// 慢指针
 		int i = 0;
 		for (int j = 1; j < array.length; j++) {
-			// “旧数组”的指针不断往前跑，没遇到重复的，就给“新数组”赋值
+			// 快指针不断前进，遇到与慢指针数字相同的情况，跳过
 			if (array[j] != array[i]) {
 				i++;
 				array[i] = array[j];
 			}
 		}
-		array = Arrays.copyOf(array, i + 1);
-		return array.length;
-	}
-
-	// 在同一份数组空间中操作，返回新数组的长度
-	// 比上面的效率差很多
-	public static int removeDuplicates_2(int[] array) {
-		// 入参保护
-		if (array == null) {
-			return -1;
-		}
-		// 肯定不存在重复的情况
-		if (array.length < 2) {
-			return array.length;
-		}
-		// 重复的个数
-		int repeatNumber = 0;
-		// 循环自第二项开启，至新数组长度结束
-		for (int i = 1; i < array.length - repeatNumber; i++) {
-			if (array[i] == array[i - 1]) {
-				// 先加repeatNumber
-				repeatNumber++;
-				// 自下一项起，往前挪
-				for (int j = i; j < array.length - repeatNumber; j++) {
-					array[j] = array[j + 1];
-				}
-				// 向前挪完之后，下一次循环还是从当前项开始
-				i--;
-			}
-		}
-		// 去除数组多余项
-		array = Arrays.copyOf(array, array.length - repeatNumber);
-		return array.length;
-	}
-
-	// 多使用一份数组空间，但具有更高的效率
-	// 因为操作不同的内存，所以要返回指定的新数组
-	public static int[] removeDuplicates_3(int[] array) {
-		// 入参保护
-		if (array == null) {
-			return null;
-		}
-		// 肯定不存在重复的情况
-		if (array.length < 2) {
-			return array;
-		}
-		// 重复的个数
-		int repeatNumber = 0;
-		int[] newArray = new int[array.length];
-		// 第一项先赋值
-		newArray[0] = array[0];
-		for (int i = 1; i < array.length; i++) {
-			if (array[i] != array[i - 1]) {
-				newArray[i - repeatNumber] = array[i];
-			} else {
-				repeatNumber++;
-			}
-		}
-		array = Arrays.copyOf(newArray, array.length - repeatNumber);
-		return array;
+		// 返回新数组的长度，不关心之后的内容
+		return i + 1;
 	}
 }
