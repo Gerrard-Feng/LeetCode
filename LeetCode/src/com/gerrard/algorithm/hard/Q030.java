776ad29274a07fb231cd9116a369a7938068438a
ZXW-20161112YIS\Administrator <kevin_gerrard@sina.com>
2017-02-21 00:56:45 +0800

    temp

--- LeetCode/src/com/gerrard/algorithm/hard/Q030.java
+++ LeetCode/src/com/gerrard/algorithm/hard/Q030.java
@@ -44,10 +44,147 @@ public class Q030 {
 	}
 
 	public static List<Integer> findSubstring(String s, String[] words) {
-		List<Integer> result = new LinkedList<>();
-		if (words == null || s == null || words.length == 0) {
-			return result;
+		int N = s.length();
+		List<Integer> indexes = new ArrayList<Integer>(s.length());
+		if (words.length == 0) {
+			return indexes;
 		}
+		int M = words[0].length();
+		if (N < M * words.length) {
+			return indexes;
+		}
+		int last = N - M + 1;
+
+		// map each string in words array to some index and compute target
+		// counters
+		Map<String, Integer> mapping = new HashMap<String, Integer>(words.length);
+		int[][] table = new int[2][words.length];
+		int failures = 0, index = 0;
+		for (int i = 0; i < words.length; ++i) {
+			Integer mapped = mapping.get(words[i]);
+			if (mapped == null) {
+				++failures;
+				mapping.put(words[i], index);
+				mapped = index++;
+			}
+			++table[0][mapped];
+		}
+
+		// find all occurrences at string S and map them to their current
+		// integer, -1 means no such string is in words array
+		int[] smapping = new int[last];
+		for (int i = 0; i < last; ++i) {
+			String section = s.substring(i, i + M);
+			Integer mapped = mapping.get(section);
+			if (mapped == null) {
+				smapping[i] = -1;
+			} else {
+				smapping[i] = mapped;
+			}
+		}
+
+		// fix the number of linear scans
+		for (int i = 0; i < M; ++i) {
+			// reset scan variables
+			int currentFailures = failures; // number of current mismatches
+			int left = i, right = i;
+			Arrays.fill(table[1], 0);
+			// here, simple solve the minimum-window-substring problem
+			while (right < last) {
+				while (currentFailures > 0 && right < last) {
+					int target = smapping[right];
+					if (target != -1 && ++table[1][target] == table[0][target]) {
+						--currentFailures;
+					}
+					right += M;
+				}
+				while (currentFailures == 0 && left < right) {
+					int target = smapping[left];
+					if (target != -1 && --table[1][target] == table[0][target] - 1) {
+						int length = right - left;
+						// instead of checking every window, we know exactly the
+						// length we want
+						if ((length / M) == words.length) {
+							indexes.add(left);
+						}
+						++currentFailures;
+					}
+					left += M;
+				}
+			}
+
+		}
+		return indexes;
+	}
+
+	// 每次前移一位，效率极低
+	public static List<Integer> method_1(String s, String[] words) {
+		List<Integer> result = new LinkedList<>();
+		// 数组转集合
+		List<String> wordList = Arrays.asList(words);
+		// 单个字符串长度
+		int len = words[0].length();
+		// 匹配总长度
+		int total = len * words.length;
+		// 循环结束条件：剩余长度不够
+		for (int i = 0; i < s.length() + 1 - total; i++) {
+			// 准备具体操作的副本
+			List<String> copy = new LinkedList<>(wordList);
+			int index = i;
+			while (copy.size() != 0) {
+				String cur = s.substring(index, index + len);
+				if (!copy.contains(cur)) {
+					break;
+				}
+				copy.remove(cur);
+				index += len;
+			}
+			// 对比预期长度
+			if (index == i + total) {
+				result.add(i);
+			}
+		}
+		return result;
+	}
+
+	// 类似 method_1 逻辑，使用 Map作为容器
+	// 效率较 method_1 高很多，尤其在某些极端情况下
+	// 比如 words 长度很大，且全部由一个字符串组成
+	public static List<Integer> method_2(String s, String[] words) {
+		List<Integer> result = new LinkedList<>();
+		// 匹配数组转成 Map 键值对存储：字符串-出现次数
+		Map<String, Integer> map = new HashMap<>();
+		for (String word : words) {
+			if (map.containsKey(word)) {
+				map.put(word, map.get(word) + 1);
+			} else {
+				map.put(word, 1);
+			}
+		}
+		int len = words[0].length();
+		int total = len * words.length;
+		for (int i = 0; i < s.length() + 1 - total; i++) {
+			Map<String, Integer> copy = new HashMap<>(map);
+			int index = i;
+			int mapLen = words.length;
+			while (mapLen != 0) {
+				String cur = s.substring(index, index + len);
+				if (!copy.containsKey(cur) || copy.get(cur) == 0) {
+					break;
+				}
+				copy.put(cur, copy.get(cur) - 1);
+				index += len;
+				mapLen--;
+			}
+			if (index == i + total) {
+				result.add(i);
+			}
+		}
+		return result;
+	}
+
+	public static List<Integer> method_3(String s, String[] words) {
+		List<Integer> result = new LinkedList<>();
 		// 匹配数组转成 Map 键值对存储：字符串-出现次数
 		Map<String, Integer> initMap = new HashMap<>();
 		Map<String, Integer> initScanned = new HashMap<>();
@@ -111,16 +248,8 @@ public class Q030 {
 		}
 		return result;
 	}
-	
-	private static int calcScanned(Map<String, Integer> scannedMap) {
-		int result = 0;
-		for (String key : scannedMap.keySet()) {
-			result += scannedMap.get(key);
-		}
-		return result;
-	}
-	
-	public static List<Integer> method_3(String s, String[] words) {
+
+	public static List<Integer> method_4(String s, String[] words) {
 		List<Integer> result = new LinkedList<>();
 		if (words == null || s == null || words.length == 0) {
 			return result;
@@ -184,83 +313,19 @@ public class Q030 {
 		}
 		return result;
 	}
-	
+
+	private static int calcScanned(Map<String, Integer> scannedMap) {
+		int result = 0;
+		for (String key : scannedMap.keySet()) {
+			result += scannedMap.get(key);
+		}
+		return result;
+	}
+
 	private static boolean containsMap(Map<String, Integer> map, String str) {
 		if (!map.containsKey(str) || map.get(str) == 0) {
 			return false;
 		}
 		return true;
-	}
-
-	// 每次前移一位，效率极低
-	public static List<Integer> method_1(String s, String[] words) {
-		List<Integer> result = new LinkedList<>();
-		if (words == null || s == null || words.length == 0) {
-			return result;
-		}
-		// 数组转集合
-		List<String> wordList = Arrays.asList(words);
-		// 单个字符串长度
-		int len = words[0].length();
-		// 匹配总长度
-		int total = len * words.length;
-		// 循环结束条件：剩余长度不够
-		for (int i = 0; i < s.length() + 1 - total; i++) {
-			// 准备具体操作的副本
-			List<String> copy = new LinkedList<>(wordList);
-			int index = i;
-			while (copy.size() != 0) {
-				String cur = s.substring(index, index + len);
-				if (!copy.contains(cur)) {
-					break;
-				}
-				copy.remove(cur);
-				index += len;
-			}
-			// 对比预期长度
-			if (index == i + total) {
-				result.add(i);
-			}
-		}
-		return result;
-	}
-
-	// 类似 method_1 逻辑，使用 Map作为容器
-	// 效率较 method_1 高很多，尤其在某些极端情况下
-	// 比如 words 长度很大，且全部由一个字符串组成
-	public static List<Integer> method_2(String s, String[] words) {
-		List<Integer> result = new LinkedList<>();
-		if (words == null || s == null || words.length == 0) {
-			return result;
-		}
-		// 匹配数组转成 Map 键值对存储：字符串-出现次数
-		Map<String, Integer> map = new HashMap<>();
-		for (String word : words) {
-			if (map.containsKey(word)) {
-				map.put(word, map.get(word) + 1);
-			} else {
-				map.put(word, 1);
-			}
-		}
-		int len = words[0].length();
-		int total = len * words.length;
-		for (int i = 0; i < s.length() + 1 - total; i++) {
-			Map<String, Integer> copy = new HashMap<>(map);
-			int index = i;
-			int mapLen = words.length;
-			while (mapLen != 0) {
-				String cur = s.substring(index, index + len);
-				if (!copy.containsKey(cur) || copy.get(cur) == 0) {
-					break;
-				}
-				copy.put(cur, copy.get(cur) - 1);
-				index += len;
-				mapLen--;
-			}
-			if (index == i + total) {
-				result.add(i);
-			}
-		}
-		return result;
 	}
 }
